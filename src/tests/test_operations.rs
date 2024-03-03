ca47c8995a9ddb1987f548e7aec117ecd0ed8fbc
rn-santos227 <rn_santos227@yahoo.com>
2024-03-03 13:03:21 +0800

    update test_operations.rd

--- src/tests/test_operations.rs
+++ src/tests/test_operations.rs
@@ -26,25 +26,35 @@ mod tests {
     #[test]
     fn test_add() {
         assert_eq!(add(1, 2), 3);
+        assert_eq!(add(4, 4), 8);
+        assert_eq!(add(3, 4), 7);
     }
 
     #[test]
     fn test_subtract() {
         assert_eq!(subtract(4, 2), 2);
+        assert_eq!(subtract(10, 2), 8);
+        assert_eq!(subtract(9, 4), 5);
     }
 
     #[test]
     fn test_multiply() {
         assert_eq!(multiply(6, 6), 36);
+        assert_eq!(multiply(4, 2), 8);
+        assert_eq!(multiply(9, 8), 72);
     }
 
     #[test]
     fn test_divide() {
         assert_eq!(divide(10, 5), 2.0);
+        assert_eq!(divide(20, 4), 5.0);
+        assert_eq!(divide(81, 3), 27.0);
     }
 
     #[test]
     fn test_modulus() {
         assert_eq!(modulus(5, 2), 1);
+        assert_eq!(modulus(11, 3), 2);
+        assert_eq!(modulus(29, 5), 4);
     }
 }
